b3294e0dc5f7898f772b690f6b2978db1632c7e7
chojaeho11 <korea908as@gmail.com>
2026-03-18 10:07:48 +0900

    fix: update Toss clientKey to new widget key from dashboard

--- site-config.js
+++ site-config.js
@@ -81,7 +81,7 @@ export const SITE_CONFIG = {
     PG_CONFIG: {
         'KR': {
             provider: 'toss',
-            clientKey: 'live_ck_4yKeq5bgrpLgoDjOgjeBrGX0lzW6',
+            clientKey: 'live_gck_DpexMgkW36PiL65R5Y4ErGbR5ozO',
             stripeKeyForSub: 'pk_live_51SfcQ79Uc8Z1bGiuqdFz7CmDXn7Ga7HAkf7XUxsyyvsMWbXTNSS3AMRRoXFS8U1EhTFeBsqX4Axb79Nwig8Lohzs00PIMNcGtG'
         },
         'JP': {
